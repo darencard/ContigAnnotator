114e56148750029b7c20906292c37f218e90cb91
Daren Card <darencard@daren-macpro-erb444.uta.edu>
2014-08-29 12:27:12 -0500

    Added ability to input multiple blast outputs into annotation dictionary creation, with script annotations for manual addition of more (if desireable). User can also now rank the blast outputs based upon his/her confidence in the homology it is inferring, so that more confident homology is annotated before less confident homology (with homology type indicated).

--- make_annotation_dictionary.py
+++ make_annotation_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/local/env python
 
-#print(__name__
+#print__name__
 
 from __future__ import print_function
 import optparse
@@ -19,12 +19,15 @@ which are meant to confidently infer homology, and creates an annotation diction
 dictionary is simply a python dictionary that is outputted in json format for easy incorporation into \
 other scripts, so that time-consuming dictionary creation doesn't have to be rerun multiple times. \
 This script is meant to be run before the 'annotate_counts.py' and 'annotate_fasta.py' scripts, \
-which actually annotate count (expression) tables and genome/transcriptome assembly contigs (fasta). \
+which actually annotate count (expression) tables and genome/transcriptome assembly contigs (fasta), respectively. \
 Input is tabular output from a reciprocal best-hit and/or one-way blast between the genome/transcriptome of \
 interest and a high-quality, annotated genome with target contig IDs as column 1 and reference contig \
-IDs as column 2. Output is a json formatted version of the dictionary that may be named by the user.
+IDs as column 2. Multiple input blast files can be designated - up to 3 reciprocal best-hit and 3 one-way
+best-hit (script is annotated to allow addition of more) - and the user must specify the order in which they \
+should be considered (i.e., rank the confidence of homology between blast inputs). Output is a json formatted \
+version of the dictionary that may be named by the user. Errors will be written to STDOUT if user input is missing!
 
-python make_annotation_dictionary.py -r <RBH_output> -b <one-way_output> [-o <dictionary>]"""
+python make_annotation_dictionary.py --confidence rN,bN [--rN <RBH_output> --bN <one-way_output> --out <dictionary>]"""
 
 
 #################################################
@@ -34,9 +37,15 @@ python make_annotation_dictionary.py -r <RBH_output> -b <one-way_output> [-o <di
 usage = usage_line
                         
 parser = optparse.OptionParser(usage=usage)
-parser.add_option("-r", action="store", type = "string", dest = "reciprocal", help = "reciprocal best-blast output")
-parser.add_option("-b", action="store", type = "string", dest = "oneway", help = "one-way best-blast output")
-parser.add_option("-o", action="store", type = "string", dest = "output", help = "output dictionary", default = "assembly_dictionary.json")
+parser.add_option("--r1", action = "store", type = "string", dest = "reciprocal1", help = "reciprocal best-blast output")
+parser.add_option("--b1", action = "store", type = "string", dest = "oneway1", help = "one-way best-blast output")
+parser.add_option("--r2", action = "store", type = "string", dest = "reciprocal2", help = "reciprocal best-blast output")
+parser.add_option("--b2", action = "store", type = "string", dest = "oneway2", help = "one-way best-blast output")
+parser.add_option("--r3", action = "store", type = "string", dest = "reciprocal3", help = "reciprocal best-blast output")
+parser.add_option("--b3", action = "store", type = "string", dest = "oneway3", help = "one-way best-blast output")
+## Note: To allow for more reciprocal/one-way blast input, copy the above definition(s) and adjust accordingly (e.g., --b3 -> --bN) ##
+parser.add_option("--out", action = "store", type = "string", dest = "output", help = "output dictionary", default = "assembly_dictionary.json")
+parser.add_option("--confidence", action = "store", type = "string", dest = "conf", help = "rank homology confidence of each blast input")
 
 options, args = parser.parse_args()
 
@@ -47,41 +56,103 @@ annotation_dict = {}
 ###           	Read RBH output	   	          ###
 #################################################
 	
-def rbh_in():
-	if options.reciprocal is None:									## If no rbh output was specified
-		print("\n***Error: specify reciprocal best-blast output!***\n")
+def r1_in():
+	if options.reciprocal1 is None:									## If no rbh output was specified
+		print("\n\n***Error: specify reciprocal best-blast output!***\n***Error: outputted assembly dictionary is incomplete!***")
 	else:															## If necessary rbh output was specified
 		total = 0
-		print("\n***Parsing reciprocal best-blast output***\n")
-		for foo in open(options.reciprocal).read().splitlines():	## Open file and split by lines
+		print("\n\n***Parsing reciprocal best-blast output***\n")
+		for foo in open(options.reciprocal1).read().splitlines():	## Open file and split by lines
 			total += 1
 			print(total, end='\r')
 			bar = foo.split()										## Split by tabs
 			denovo = bar[0]											## de novo transcriptome ID
-			ensembl = bar[1]+"_rbh_"bar[3]							## blast match Ensembl ID
+			ensembl = bar[1]+"_rbh1_"+bar[2]							## blast match Ensembl ID
+			if denovo not in annotation_dict.keys():				## If de novo ID not already in keys
+				annotation_dict[denovo] = ensembl					## Put de novo id as key and ensembl id as value
+
+def r2_in():
+	if options.reciprocal2 is None:									## If no rbh output was specified
+		print("\n\n***Error: specify reciprocal best-blast output!***\n***Error: outputted assembly dictionary is incomplete!***")
+	else:															## If necessary rbh output was specified
+		total = 0
+		print("\n\n***Parsing reciprocal best-blast output***\n")
+		for foo in open(options.reciprocal2).read().splitlines():	## Open file and split by lines
+			total += 1
+			print(total, end='\r')
+			bar = foo.split()										## Split by tabs
+			denovo = bar[0]											## de novo transcriptome ID
+			ensembl = bar[1]+"_rbh2_"+bar[2]							## blast match Ensembl ID
+			if denovo not in annotation_dict.keys():				## If de novo ID not already in keys
+				annotation_dict[denovo] = ensembl					## Put de novo id as key and ensembl id as value
+				
+def r3_in():
+	if options.reciprocal3 is None:									## If no rbh output was specified
+		print("\n\n***Error: specify reciprocal best-blast output!***\n***Error: outputted assembly dictionary is incomplete!***")
+	else:															## If necessary rbh output was specified
+		total = 0
+		print("\n\n***Parsing reciprocal best-blast output***\n")
+		for foo in open(options.reciprocal3).read().splitlines():	## Open file and split by lines
+			total += 1
+			print(total, end='\r')
+			bar = foo.split()										## Split by tabs
+			denovo = bar[0]											## de novo transcriptome ID
+			ensembl = bar[1]+"_rbh3_"+bar[2]							## blast match Ensembl ID
 			if denovo not in annotation_dict.keys():				## If de novo ID not already in keys
 				annotation_dict[denovo] = ensembl					## Put de novo id as key and ensembl id as value
 		
+## Note: To allow for more reciprocal blast input, copy the above definition and adjust it accordingly (e.g., r3_in -> rN_in) ##
 
 #################################################
 ###         Read one-way blast output         ###
 #################################################
 
-def oneway_in():
-	if options.oneway is None:										## If no oneway output was specified
-		print("\n***Error: specify one-way best-blast output!***\n")
+def b1_in():
+	if options.oneway1 is None:										## If no oneway output was specified
+		print("\n\n***Error: specify one-way best-blast output!***\n***Error: outputted assembly dictionary is incomplete!***")
 	else:															## If necessary rbh output was specified	
 		total = 0
 		print("\n\n***Parsing oneway best-blast output***\n")
-		for foo in open(options.oneway).read().splitlines():		## Open file and split by lines
+		for foo in open(options.oneway1).read().splitlines():		## Open file and split by lines
 			total += 1
 			print(total, end='\r')
 			bar = foo.split()										## Split by tabs
 			denovo = bar[0]											## de novo transcriptome ID
-			ensembl = bar[1]+"_one_"bar[3]
+			ensembl = bar[1]+"_one1_"+bar[2]
+			if denovo not in annotation_dict.keys():				## If de novo ID not already in keys
+				annotation_dict[denovo] = ensembl					## Put de novo id as key and ensembl id as value
+				
+def b2_in():
+	if options.oneway2 is None:										## If no oneway output was specified
+		print("\n\n***Error: specify one-way best-blast output!***\n***Error: outputted assembly dictionary is incomplete!***")
+	else:															## If necessary rbh output was specified	
+		total = 0
+		print("\n\n***Parsing oneway best-blast output***\n")
+		for foo in open(options.oneway2).read().splitlines():		## Open file and split by lines
+			total += 1
+			print(total, end='\r')
+			bar = foo.split()										## Split by tabs
+			denovo = bar[0]											## de novo transcriptome ID
+			ensembl = bar[1]+"_one2_"+bar[2]
+			if denovo not in annotation_dict.keys():				## If de novo ID not already in keys
+				annotation_dict[denovo] = ensembl					## Put de novo id as key and ensembl id as value
+				
+def b3_in():
+	if options.oneway3 is None:										## If no oneway output was specified
+		print("\n\n***Error: specify one-way best-blast output!***\n***Error: outputted assembly dictionary is incomplete!***")
+	else:															## If necessary rbh output was specified	
+		total = 0
+		print("\n\n***Parsing oneway best-blast output***\n")
+		for foo in open(options.oneway3).read().splitlines():		## Open file and split by lines
+			total += 1
+			print(total, end='\r')
+			bar = foo.split()										## Split by tabs
+			denovo = bar[0]											## de novo transcriptome ID
+			ensembl = bar[1]+"_one3_"+bar[2]
 			if denovo not in annotation_dict.keys():				## If de novo ID not already in keys
 				annotation_dict[denovo] = ensembl					## Put de novo id as key and ensembl id as value
 
+## Note: To allow for more one-way blast input, copy the above definition and adjust it accordingly (e.g., r3_in -> rN_in) ##
 
 #################################################
 ###         	  Store dictionary            ###
@@ -92,7 +163,14 @@ def store_dict():
 	with open(options.output, "wb") as outfile:						
 		json.dump(annotation_dict, outfile)
 
-				
-rbh_in()
-oneway_in()
-store_dict()
+def main():
+	if options.conf is None:
+		print("\n***Error: specify the homology confidence ranking for all of your blast input files!***\n")
+	else:
+		order = options.conf.split(",")
+		for x in range(0, len(order)):
+			line = str(order[x])+"_in()"
+			eval(line)
+		store_dict()
+
+main()
